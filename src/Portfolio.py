8ee6a3a0c8f3bef0d9855f1990f63faf2f447181
Ryan Levine <rlevine9@binghamton.edu>
2021-06-26 18:19:11 -0400

    Updated and added methods to Portfolio and StockPosition classes

--- src/Portfolio.py
+++ src/Portfolio.py
@@ -7,43 +7,58 @@ class Portfolio():
 		self.cash = cash
 
 	def buyShares(self, newTicker, numShares):
-		cost = get_live_price(newTicker) * numShares
-		if cost < cash
-			initiate = True
-			addIndex = -1
-			for x in self.Positions:
-				if x.ticker == newTicker:
-					initiate = False
-					foundStock = x
-		
-			if initiate:
-				self.stockPositions.append(newTicker, numShares)
+		if self.verifyPurchase(ticker, numSharesBought):
+			self.cash -= get_live_price(ticker) * numSharesBought
+			if self.findStockIndex(ticker) != -1:
+				self.stockPositions[self.findStockIndex(ticker)].addToPos(numSharesBought)
 			else:
-				foundStock.addToPos(numShares)
+				self.StockPositions.append(StockPosition(ticker, numSharesBought))
 	
 	def sellShares(self, ticker, numSharesSold):
-		for stock in self.StockPositions:
-			if stock.ticker == ticker:
-				stock.numShares -= numSharesSold
-			proceeds = numSharesSold * get_live_price(ticker)
-			self.cash += proceeds
-			
+		index = self.findStockIndex(ticker)
+		if verifySale(ticker, numShares):
+			StockPositions[index].numShares -= numSharesSold
+			self.cash += numSharesSold * get_live_price(ticker)
+			if StockPositions[index].numSharesSold == 0:
+				StockPositions.remove(StockPositions[index])
+				
 	def getValue():
 		value = self.cash
 		for stock in self.StockPositions:
-			value += (get_live_price(stock.ticker) * stock.numShares)
-			
+			value += (get_live_price(stock.ticker) * stock.numShares)		
 		return value
 	
-	def depositCash(amount):
+	def deposit(amount):
 		self.cash += amount
 	
-	def withdrawCash(amount):
+	def withdraw(amount):
 		if amount > self.cash:
 			return False
 		else:
 			cash -= amount
 			return True
+			
+	def verifyPurchase(self, ticker, numSharesBought):
+		if self.cash >= (get_live_price(ticker) * numSharesBought):
+			return True
+		return False
+		
+	def verifySale(self, ticker, numShares):
+		index = self.findStockIndex(ticker)
+		if index == -1:
+			return False
+		elif self.StockPositions[index].numShares < numShares:
+			return False
+		return True
+	
+	def findStockIndex(self, ticker):
+		index  = -1
+		for x in range(len(self.StockPositions)):
+			if self.StockPositions[x].ticker == ticker:
+				index = x 
+		return index
+			
+	
 			
 
 
--- src/stockPosition.py
+++ src/stockPosition.py
@@ -9,3 +9,10 @@ class StockPosition():
 	def addToPos(self, numSharesAdd):
 		self.numShares += numSharesAdd
 		self.cost += (numSharesAdd * get_live_price(ticker))
+		
+	def calcGL(self):
+		standing = (get_live_price(self.ticker) * self.numShares) - self.cost
+		return standing
+		
+	def calcPercentGL(self):
+		return (((calcStanding() - self.cost)/self.cost) * 100)
