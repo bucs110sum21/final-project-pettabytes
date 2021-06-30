25007af44dbedf47f0d99e1cec0b60625670aa63
Ryan Levine <rlevine9@binghamton.edu>
2021-06-29 20:59:45 -0400

    Added descriptions for all class methods

--- src/Portfolio.py
+++ src/Portfolio.py
@@ -2,10 +2,17 @@ from yahoo_fin.stock_info import *
 from StockPosition import *
 
 class Portfolio():
+	
+	##Creates a single portfolio object. One portfolio object contains a list of StockPosition objects within it that make up the portfolio.
+	##A portfolio object also contains a double value 'cash' which represents the user's cash balance.
 	def __init__(self, cash):
 		self.StockPositions = []
 		self.cash = cash
 
+##buyShares: simulates the action of buying a certain number of shares of a given company.
+##First it verifies that the purchase can be made by calling the verifyPurchase function with the ticker(letter symbol) of the stock that 
+##that the user wants to purchase. If the purchase is verified then the function either adds a new StockPosition object to the Portfolio object's
+##list or adds a given number of shares to an existing stock position. It then subtracts the cost of this purchase from the 'cash' variable.
 	def buyShares(self, newTicker, numShares):
 		if self.verifyPurchase(ticker, numSharesBought):
 			self.cash -= get_live_price(ticker) * numSharesBought
@@ -13,7 +20,11 @@ class Portfolio():
 				self.stockPositions[self.findStockIndex(ticker)].addToPos(numSharesBought)
 			else:
 				self.StockPositions.append(StockPosition(ticker, numSharesBought))
-	
+
+##sellShares: simulates the action of selling a certain number of shares of given company.
+##First if verifies that the sale can take place by calling the verifySale function. If the sale is verified, then the given number of shares 
+##are removed from the corresponding stockPosition in the list. If the user sold all shares, then the stockPosition is removed from the list.
+##Finally, it adds the proceeds to the cash variable.
 	def sellShares(self, ticker, numSharesSold):
 		index = self.findStockIndex(ticker)
 		if verifySale(ticker, numShares):
@@ -21,28 +32,39 @@ class Portfolio():
 			self.cash += numSharesSold * get_live_price(ticker)
 			if StockPositions[index].numSharesSold == 0:
 				StockPositions.remove(StockPositions[index])
-				
-	def calcValue():
+
+##calcValue: returns the total value of the user's "account"
+##Loops through the Portfolio object's list and gets the current value of each StockPosition and keeps a running total. Then adds this total
+##to the value of the cash variable.			
+	def calcValue(self):
 		value = self.cash
 		for stock in self.StockPositions:
 			value += (get_live_price(stock.ticker) * stock.numShares)		
 		return value
 	
-	def deposit(amount):
+##deposit: Simulates depositing cash into the account.
+##increases the Portfolio object's cash balance by the 'amount' entered.
+	def deposit(self, amount):
 		self.cash += amount
-	
-	def withdraw(amount):
+
+##withdraw: Simulates withdrawing cash from the account.
+##Decreases the cash variable by 'amount' entered as long as it is less than the current cash balance.
+	def withdraw(self, amount):
 		if amount > self.cash:
-			return False
-		else:
 			cash -= amount
-			return True
-			
+
+##verifyPurchase: Returns a boolean value that indicates if the purchase described can take place.
+##If the total cost of the transaction is less than or equal to the current cash balance, it returns true.
+##If not, it returns false.		
 	def verifyPurchase(self, ticker, numSharesBought):
 		if self.cash >= (get_live_price(ticker) * numSharesBought):
 			return True
 		return False
-		
+
+
+##verifySale: Returns a boolean value that indicates if the sale described can take place.	
+##If the stock entered exists in the portfolio and the number of shares entered is less than the number of shares stored by the StockPosition
+##object in question, then it will return True. If not, then it returns False.	
 	def verifySale(self, ticker, numShares):
 		index = self.findStockIndex(ticker)
 		if index == -1:
@@ -50,27 +72,31 @@ class Portfolio():
 		elif self.StockPositions[index].numShares < numShares:
 			return False
 		return True
-	
+
+##findStockIndex: finds the index of the stock with the given ticker in the Portfolio object's StockPositions[] list 	
 	def findStockIndex(self, ticker):
 		index  = -1
 		for x in range(len(self.StockPositions)):
 			if self.StockPositions[x].ticker == ticker:
 				index = x 
 		return index
-		
+
+##calcPortGL: returns the Portfolio objects current gain or loss by subtracting current value from the sum of each stockPosition object's cost.		
 	def calcPortGL(self):
 		GL = 0
 		for x in self.StockPositions:
 			GL += x.calcGL()
 		return GL
-	
+
+##calcPortCost: returns the total cost of all StockPostion objects in the Portfolio object's list ('StockPosition'[])
+##Loops through the list and keeps a running total of each StockPosition's cost	
 	def calcPortCost(self):
 		portCost = 0
 		for x in StockPositions:
 			portCost += x.cost
 		return portCost
 		
-	
+##calcPortPercentGL: returns the Portfolio's gain or loss as a percentage of its total cost	
 	def calcPortPercentGL(self):
 		GL = self.calcPortPercentGL()
 		cost = self.calcPortCost()
--- src/stockPosition.py
+++ src/stockPosition.py
@@ -1,18 +1,29 @@
 from yahoo_fin.stock_info import *
 
 class StockPosition():
+
+##YAHOO-FIN module note:
+##	'get_live_price(ticker)' function gets the live price of a stock from yahoo finance. The parameter 'ticker' refers to the letter symbol of the stock.
+
+##StockPosition: Each StockPosition object is meant to represent a stock that the user owns.
+##'ticker' is a str variable that should contain the ticker (ex: Microsoft -> MSFT, Tesla -> TSLA) of the stock being referenced.
+##'numShares' is a float variable that contains the number of shares of a given stock that are owned by the user.
+##'cost' is a float variable that contains the total cost of the StockPosition
 	def __init__(self, ticker, numShares):
 		self.ticker = ticker
 		self.numShares = float(numShares)
 		self.cost = get_live_price(ticker) * numShares
 
+##addToPos: adds a given number of shares to the StockPosition object and increases the StockPosition's cost.
+##called when the user buys additional shares of a stock that they already own shares of.
 	def addToPos(self, numSharesAdd):
 		self.numShares += numSharesAdd
 		self.cost += (numSharesAdd * get_live_price(ticker))
-		
+
+##calcGL: returns the current gain or loss of the StockPosition object by subtracting its cost from its current value(live price * number of shares)		
 	def calcGL(self):
 		standing = (get_live_price(self.ticker) * self.numShares) - self.cost
 		return standing
-		
+##calcPercentGL: returns the current gain or loss as a percent of the StockPosition object's cost.		
 	def calcPercentGL(self):
 		return (((calcStanding() - self.cost)/self.cost) * 100)
